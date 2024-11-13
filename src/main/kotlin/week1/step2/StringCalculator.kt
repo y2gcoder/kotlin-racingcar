0c8ed9775ca1ad56693ada7e5755196d5327a86c
y2gcoder <y2gcoder@gmail.com>
2024-11-13 17:39:35 +0900

    feature(step2): Initialize StringCalculator V0.1
    문자열계산기(StringCalculator)를 초기에 구현한 내용입니다.
    테스트 코드를 통과하기 위한 코드이며, 테스트 코드를 바탕으로 리팩토링할 예정입니다.
    테스트 케이스 또한 추가될 수 있습니다.

---
+++ src/main/kotlin/week1/step2/StringCalculator.kt
@@ -0,0 +1,39 @@
+package week1.step2
+
+class StringCalculator {
+    fun calculate(input: String): Double {
+        val trimmedInput = input.trim()
+        if (trimmedInput.isEmpty()) {
+            throw IllegalArgumentException("입력값은 빈 문자열일 수 없습니다")
+        }
+
+        // 첫번째 숫자 앞에 + 문자열 추가
+        val targetInput = "+ $trimmedInput"
+
+        // 문자열 파싱
+        val parts = targetInput.split(" ")
+
+        var result = 0.0
+
+        // 2개씩 (operator , number)
+        for (i in parts.indices.filter { i -> i % 2 == 0 }) {
+            val numberString = parts[i + 1]
+            val number = numberString.toDouble()
+            val operationString = parts[i]
+
+            if (operationString == "+") {
+                result += number
+            } else if (operationString == "-") {
+                result -= number
+            } else if (operationString == "*") {
+                result *= number
+            } else if (operationString == "/") {
+                result /= number
+            } else {
+                throw IllegalArgumentException("올바른 사칙 연산 기호가 아닙니다: operatorString=$operationString")
+            }
+        }
+
+        return result
+    }
+}
