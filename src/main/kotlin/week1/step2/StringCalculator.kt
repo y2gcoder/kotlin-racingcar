9a11a494c4a4f6db78d21028f866c417667efd8d
y2gcoder <y2gcoder@gmail.com>
2024-11-13 23:08:24 +0900

    feature(step2): Add Operation and Operations and extract methods
    연산을 수행하기 위한 역할을 하는 Operation 추가
    그리고 해당 Operation 들을 모아 연산을 순차적으로 실행해주는 Operations 추가
    Operations 를 만들기 위한 validation, parsing inputs, creating operation list 등을 각각 수행하는 메서드 추출도 적용

--- src/main/kotlin/week1/step2/StringCalculator.kt
+++ src/main/kotlin/week1/step2/StringCalculator.kt
@@ -2,29 +2,49 @@ package week1.step2
 
 class StringCalculator {
     fun calculate(input: String): Double {
-        val trimmedInput = input.trim()
-        if (trimmedInput.isEmpty()) {
+        val parts = parseInput(input)
+        val operations = createOperations(parts)
+        return operations.executeAll()
+    }
+
+    private fun parseInput(input: String): List<String> {
+        if (input.isBlank()) {
             throw IllegalArgumentException("입력값은 빈 문자열일 수 없습니다")
         }
 
-        // 첫번째 숫자 앞에 + 문자열 추가
-        val targetInput = "+ $trimmedInput"
-
-        // 문자열 파싱
-        val parts = targetInput.split(" ")
-
-        var result = 0.0
-
-        // 2개씩 (operator , number)
-        for (i in parts.indices.filter { i -> i % 2 == 0 }) {
-            val numberString = parts[i + 1]
-            val number = numberString.toDouble()
-            val operatorSymbol = parts[i]
-
-            result = Operator.fromSymbol(operatorSymbol).apply(result, number)
+        val parts = input.trim().split(" ")
+        if (parts.size % 2 == 0) {
+            throw IllegalArgumentException("올바르지 않은 형식의 계산식입니다: $input")
         }
 
-        return result
+        return listOf("+") + parts
+    }
+
+    private fun createOperations(parts: List<String>): Operations {
+        return Operations(
+            parts.chunked(2)
+                .map { chunk ->
+                    val operator = Operator.fromSymbol(chunk[0])
+                    val operand =
+                        chunk[1]
+                            .toDoubleOrNull()
+                            ?: throw IllegalArgumentException("피연산자는 숫자가 아닙니다: ${chunk[1]}")
+
+                    Operation(operator, operand)
+                },
+        )
+    }
+}
+
+class Operations(private val operations: List<Operation>) {
+    fun executeAll(): Double {
+        return operations.fold(0.0) { result, operation -> operation.operate(result) }
+    }
+}
+
+class Operation(private val operator: Operator, private val operand: Double) {
+    fun operate(initialValue: Double): Double {
+        return operator.apply(initialValue, operand)
     }
 }
 
