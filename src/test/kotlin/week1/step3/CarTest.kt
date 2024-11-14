816bb3d6da3f6e75fa71d2a589ddbee481f7daa7
y2gcoder <y2gcoder@gmail.com>
2024-11-15 01:05:41 +0900

    test(step3-racingcar): Modify test's text: 자동차 -> Car

--- src/test/kotlin/week1/step3/CarTest.kt
+++ src/test/kotlin/week1/step3/CarTest.kt
@@ -5,7 +5,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class CarTest : StringSpec({
-    "자동차는 4~9의 무작위 값을 받으면 한 칸 전진한다" {
+    "Car는 4~9의 무작위 값을 받으면 한 칸 전진한다" {
         listOf(4, 5, 6, 7, 8, 9).forEach { randomNumber ->
             val sut = Car()
             val initialPosition = sut.position
@@ -17,7 +17,7 @@ class CarTest : StringSpec({
         }
     }
 
-    "자동차는 0~3의 무작위 값을 받으면 그대로 멈춘다" {
+    "Car는 0~3의 무작위 값을 받으면 그대로 멈춘다" {
         listOf(0, 1, 2, 3).forEach { randomNumber ->
             val sut = Car()
             val initialPosition = sut.position
@@ -29,7 +29,7 @@ class CarTest : StringSpec({
         }
     }
 
-    "자동차는 0~9 이외의 값을 받으면 InvalidArgumentException 예외를 던진다" {
+    "Car는 0~9 이외의 값을 받으면 InvalidArgumentException 예외를 던진다" {
         listOf(-1, 10).forEach { randomNumber ->
             val sut = Car()
 
