c67cbf61cb44d86637a3d5554e048a75259ef929
y2gcoder <y2gcoder@gmail.com>
2024-11-15 00:48:59 +0900

    feature(step3-racingcar): Add Car
    자동차 경주 게임 중 자동차 클래스 구현.
    랜덤한 숫자를 받아 조건에 따라 움직이는 move()에 대한 테스트 코드 작성.(Kotest로 작성)

---
+++ src/main/kotlin/week1/step3/Car.kt
@@ -0,0 +1,21 @@
+package week1.step3
+
+class Car {
+    var position: Int = 0
+
+    fun move(randomNumber: Int) {
+        if (STOPPING_NUMBERS.contains(randomNumber)) {
+            return
+        }
+        if (FORWARDING_NUMBERS.contains(randomNumber)) {
+            position++
+            return
+        }
+        throw IllegalArgumentException("0-9 사이의 randomNumber 가 아닙니다: randomNumber=$randomNumber")
+    }
+
+    companion object {
+        private val FORWARDING_NUMBERS = listOf(4, 5, 6, 7, 8, 9)
+        private val STOPPING_NUMBERS = listOf(0, 1, 2, 3)
+    }
+}
