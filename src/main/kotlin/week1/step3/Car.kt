7123bee3fdecc243af7ecb9249e0f5bd2b9cb662
y2gcoder <y2gcoder@gmail.com>
2024-11-15 01:22:46 +0900

    test(step3-racingcar): Create constant DEFAULT_POSITON in Car
    매직넘버를 피하기 위해 Car 내부에 기본 위치에 대한 상수값을 지정.

--- src/main/kotlin/week1/step3/Car.kt
+++ src/main/kotlin/week1/step3/Car.kt
@@ -1,7 +1,7 @@
 package week1.step3
 
 class Car {
-    var position: Int = 0
+    var position: Int = DEFAULT_POSITION
 
     fun move(randomNumber: Int) {
         if (STOPPING_NUMBERS.contains(randomNumber)) {
@@ -15,6 +15,7 @@ class Car {
     }
 
     companion object {
+        const val DEFAULT_POSITION = 0
         private val FORWARDING_NUMBERS = listOf(4, 5, 6, 7, 8, 9)
         private val STOPPING_NUMBERS = listOf(0, 1, 2, 3)
     }
