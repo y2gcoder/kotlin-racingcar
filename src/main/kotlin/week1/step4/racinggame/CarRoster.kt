b373ef7ff927bb52b3333d0e8b2fc228ecfbcc4b
y2gcoder <y2gcoder@gmail.com>
2024-11-22 00:22:48 +0900

    feature(racing-game): CarRoster 를 별개의 파일로 분리
    리뷰어님 피드백 반영.

---
+++ src/main/kotlin/week1/step4/racinggame/CarRoster.kt
@@ -0,0 +1,14 @@
+package week1.step4.racinggame
+
+@JvmInline
+value class CarRoster(private val carNames: List<String>) {
+    init {
+        require(carNames.isNotEmpty()) {
+            "레이싱 게임을 하기 위해서는 차가 1대 이상이어야 합니다."
+        }
+    }
+
+    fun createCars(): List<Car> {
+        return carNames.map { Car(it) }
+    }
+}
--- src/main/kotlin/week1/step4/racinggame/RacingGame.kt
+++ src/main/kotlin/week1/step4/racinggame/RacingGame.kt
@@ -2,19 +2,6 @@ package week1.step4.racinggame
 
 import kotlin.random.Random
 
-@JvmInline
-value class CarRoster(private val carNames: List<String>) {
-    init {
-        require(carNames.isNotEmpty()) {
-            "레이싱 게임을 하기 위해서는 차가 1대 이상이어야 합니다."
-        }
-    }
-
-    fun createCars(): List<Car> {
-        return carNames.map { Car(it) }
-    }
-}
-
 fun interface RandomNumberGenerator {
     fun generate(): Int
 }
